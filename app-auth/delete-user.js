90f6c01cff9aa597eb94f187210a3188337ba0f3
Jonathan LeBlanc <joncleblanc@yahoo.com>
2018-06-25 12:35:42 -0500

    Add app / managed user create and delete samples

---
+++ app-auth/delete-user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Initialize packages
+const util = require('util');            // Deep inspection of objects
+const boxSDK = require('box-node-sdk');  // Box SDK
+const fs = require('fs');                // File system for config
+
+/**************************************************************
+* Setup auth via downloaded JSON config file
+**************************************************************/
+// Fetch config file for instantiating SDK instance
+const configJSON = JSON.parse(fs.readFileSync('config.json'));
+
+// Instantiate instance of SDK using generated JSON config
+const sdk = boxSDK.getPreconfiguredInstance(configJSON);
+
+// Create service account client
+const client = sdk.getAppAuthClient('enterprise');
+
+/**************************************************************
+* Delete user
+**************************************************************/
+const userId = '3758950911';
+client.users.delete(userId).then(() => {
+  console.log(`${userId} deleted`);
+});
--- app-auth/jwt-master.js
+++ app-auth/jwt-master.js
@@ -16,7 +16,7 @@ const sdk = boxSDK.getPreconfiguredInstance(configJSON);
 
 // Create service account client
 const client = sdk.getAppAuthClient('enterprise');
-console.log(util.inspect(client, false, null));
+//console.log(util.inspect(client, false, null));
 
 /**************************************************************
 * Setup auth via manual public / private keys
@@ -42,3 +42,48 @@ const sdk = new boxSDK({
 });
 const client = sdk.getAppAuthClient('enterprise', enterpriseId);
 console.log(util.inspect(client, false, null));*/
+
+/**************************************************************
+* Create new app user
+**************************************************************/
+/*const userName = 'App User 1';
+const spaceAmount = 1073741824;   // ~ 1gb
+
+// Create app user
+client.enterprise.addAppUser(
+  userName, 
+  {
+    space_amount: spaceAmount
+  }
+).then(appUser => {
+  console.log(appUser.id);
+});*/
+
+/**************************************************************
+* Create new managed user
+**************************************************************/
+/*const userName = 'Managed User 1';
+const userEmail = 'managedusertest123@tester.com';
+const role = client.enterprise.userRoles.COADMIN;
+const address = '123 Tester Lane';
+const spaceAmount = 1073741824;   // ~ 1gb
+
+client.enterprise.addUser(
+  userEmail,
+  userName,
+  {
+    role: role,
+    address: address,
+    space_amount: spaceAmount
+  }
+).then(user => {
+  console.log(util.inspect(user.id, false, null));
+});*/
+
+/**************************************************************
+* Delete user
+**************************************************************/
+/*const userId = '3758950911';
+client.users.delete(userId).then(() => {
+  console.log(`${userId} deleted`);
+});*/
