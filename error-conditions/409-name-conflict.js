b568bd08f948b045121d3ee5be60d305186aa49a
Jonathan LeBlanc <joncleblanc@yahoo.com>
2018-06-26 14:19:44 -0500

    Add code samples for handling error condition flows

---
+++ error-conditions/409-name-conflict.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Initialize packages
+const util = require('util');            // Deep inspection of objects
+const boxSDK = require('box-node-sdk');  // Box SDK
+const fs = require('fs');                // File system for config
+
+// Fetch config file for instantiating SDK instance
+const configJSON = JSON.parse(fs.readFileSync('config.json'));
+
+// Instantiate instance of SDK using generated JSON config
+const sdk = boxSDK.getPreconfiguredInstance(configJSON);
+
+// Create service account client
+const client = sdk.getAppAuthClient('enterprise');
+
+// Upload file
+var stream = fs.createReadStream('temp.txt');
+client.files.uploadFile('0', 'tempdoc.txt', stream, callback);
+
+function callback(err, res) {
+  console.log(util.inspect(err.response.body, false, null));
+  console.log(util.inspect(res, false, null));
+}
