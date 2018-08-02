3b52d5cdb8276b989c7df98721159f53199ec40b
Jonathan LeBlanc <joncleblanc@yahoo.com>
2018-08-02 11:51:08 -0500

    Update config values

--- config.js
+++ config.js
@@ -2,21 +2,21 @@ const boxSDK = require('box-node-sdk');
 exports.boxSDK = boxSDK;
 
 // OAuth / JWT application credentials
-const jwtClientId = exports.jwtClientId = '1xy8yqzr9tyvloui0nk9mrmhgpr3c6pv';
-const jwtClientSecret = exports.jwtClientSecret = 'NGGGoDYDVTdokOUI4jGTuWA8xuQYs6hl';
+const jwtClientId = exports.jwtClientId = 'YOUR JWT APPLICATION CLIENT ID';
+const jwtClientSecret = exports.jwtClientSecret = 'YOUR JWT APPLICATION CLIENT SECRET';
 
 // OAuth application credentials
-const oauthClientId = exports.oauthClientId = 'jv0illbd5bqjwdr8pdby5y0tzwy7gdxo';
-const oauthClientSecret = exports.oauthClientSecret = 'sYbjj0AOhuNKL0RsXzR4beEjVxNqGZfP';
+const oauthClientId = exports.oauthClientId = 'YOUR OAUTH APPLICATION CLIENT ID';
+const oauthClientSecret = exports.oauthClientSecret = 'YOUR OAUTH APPLICATION CLIENT SECRET';
 
 // Account information
-const publicKeyId = exports.publicKeyId = '1h9yaj1t';
-const enterpriseId = exports.enterpriseId = '17404983';
-const userId = exports.userId = '14516989';
+const publicKeyId = exports.publicKeyId = 'YOUR APPLICATION PUBLIC KEY ID';
+const enterpriseId = exports.enterpriseId = 'YOUR ENTERPRISE';
+const userId = exports.userId = 'YOUR USER ID';
 
 // Keys
 const keyPath = exports.privateKeyPath = 'private.pem';
-const keyPass = exports.keyPass = 'e3kbsa';
+const keyPass = exports.keyPass = 'YOUR PRIVATE KEY PASSWORD';
 
 // Config file path
 const configPath = exports.configPath = 'config.json';
